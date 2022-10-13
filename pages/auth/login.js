0fd101979c16e10b166bb90e683e81145df146b2
polskisupreme <115725550+polskisupreme@users.noreply.github.com>
2022-10-13 20:33:43 +0200

    Update login.js

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,4 +1,4 @@
-import { BsGoogle } from "react-icons/Bs";
+import { FcGoogle } from "react-icons/fc";
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from "../../utils/firebase";
 import { useRouter } from 'next/router';
@@ -40,7 +40,7 @@ export default function Login() {
             <div className="py-4">
                 <h3 className="py-4 font-medium">zalguj się wybierając jedną z opcji</h3>
                 <button onClick={GoogleLogin} className="text-white bg-teal-600 w-full font-medium rounded-lg flex align-middle p-4 gap-3">
-                    <BsGoogle className="text-2xl" />
+                    <FcGoogle className="text-2xl" />
                     Zaloguj się za pomocą google
                     </button>
 
